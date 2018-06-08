a0a19341684eec2a7493bf4f8cbbf37b3414c663
Ashil John <willsquad.aj@gmail.com>
2018-06-08 09:56:21 +0530

    Contact Address Map added

--- files/js/main.js
+++ files/js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-   
+   $('.address_map_overlay').on('click', function(){
+       $('.address_map_container').addClass('slide_down_height').slideDown();
+       $(this).addClass('hide');
+   });
 });
